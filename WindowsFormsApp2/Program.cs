298ad27a0bf556775030b16d5149a0b323882871
RoMeow2112 <nguyentrankhai123@gmail.com>
2025-05-06 17:38:47 +0700

    1/2 contact

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HRForm());
+            Application.Run(new Login());
         }
     }
 }
